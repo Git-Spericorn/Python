2096543f4985218b33095518e3b16271553e4db3
aromal <aromal@spericorn.com>
2019-12-17 19:15:02 +0530

    Function prime numbers

--- sample.py
+++ sample.py
@@ -20,3 +20,27 @@ def get_user_data(self, name, date, place):
     params = {}
     response = request_retry(url, headers=headers, params=params, json_data=data, method='put')
     return response.json()
+
+
+import itertools
+
+
+def iter_primes():
+    # an iterator of all numbers between 2 and +infinity
+    numbers = itertools.count(2)
+
+    # generate primes forever
+    while True:
+        # get the first number from the iterator (always a prime)
+        prime = next(numbers)
+        yield prime
+
+        # this code iteratively builds up a chain of
+        # filters...slightly tricky, but ponder it a bit
+        numbers = filter(prime.__rmod__, numbers)
+
+
+for p in iter_primes():
+    if p > 1000:
+        break
+    print(p)
